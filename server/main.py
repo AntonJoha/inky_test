92f600f84771f30f1d869cadd6b66fcd70e2dbed
Anton Johansson <antojohansson1@gmail.com>
2022-08-02 20:26:07 +0200

    Added start of server

---
+++ server/main.py
@@ -0,0 +1,48 @@
+import os
+import queue
+from flask import Flask
+from flask import request
+import threading
+import time
+
+import inky_wrapper
+
+app = Flask(__name__)
+order = queue.Queue()
+
+
+def handle_queue():
+    while True:
+        item = order.get(block=True, timeout=None)
+    
+
+def add_image(data):
+    order.put(data, block=True, timeout=None)
+
+@app.get("/")
+def handle_info():
+    """
+    This is what's seen if someone just opens the port in the web browser 
+    """
+    return "Hej på dig"
+
+
+@app.post("/")
+def handle_start():
+    """
+    This function is called everytime your snake is entered into a game.
+    request.json contains information about the game that's about to be played.
+    """
+    data = request.get_json()
+    add_image(data)
+    
+
+    return "ok"
+
+
+
+if __name__ == "__main__":
+
+    print("Starting Battlesnake Server...")
+    port = int(os.environ.get("PORT", "8080"))
+    app.run(host="0.0.0.0", port=port, debug=True)
---
+++ server/try_inky.py
@@ -0,0 +1,34 @@
+import hitherdither
+from inky import auto
+from PIL import Image
+from get_inky import get_inky,new_inky
+from raspcheck import on_pi
+
+def _dither_image(img):
+    inky = get_inky()
+    thresholds = [64, 64, 64]  # Threshold for snapping colours, I guess?
+    palette = hitherdither.palette.Palette(inky._palette_blend(0.8, dtype='uint24'))
+    new = hitherdither.ordered.bayer.bayer_dithering(img, palette, thresholds, order=8)
+    return new.convert("P")
+
+
+def _image_from_data(data):
+    return Image.open(BytesIO(base64.b64decode(data['data'])))   
+
+def set_screen(data):
+    
+    inky = get_inky()
+    
+    image = _image_from_data(data)
+
+    
+    if on_pi():
+        image = _dither_image(image)
+
+    inky.set_image(image)
+    inky.show()
+    
+    #If it's a Mock
+    if on_pi() == False:
+        inky.wait_for_window_close()
+        new_inky()
